f7f975693f0a50f9fff13ab2ea1efce3799fc03f
ASYStent <dovgichnik@gmail.com>
2017-12-19 20:08:40 -0800

    Test2

--- src/testForQAUBER/LogIn_Page.java
+++ src/testForQAUBER/LogIn_Page.java
@@ -6,8 +6,7 @@ import org.openqa.selenium.WebElement;
 public class LogIn_Page {
     private static WebElement element = null;
 	public static final String URL = "http://ec2-52-53-181-39.us-west-1.compute.amazonaws.com/sign-in.html";
-	public static String URL1;
-	
+
     public static WebElement input_UserName(){
     	element = Exam.driver.findElement(By.xpath("//input[@name='email']"));
     	return element;
